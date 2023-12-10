b65207541acda7340cfcaf521c4ce916c8bf18d1
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2023-12-10 13:02:55 -0300

    styled mode change button dynamically

--- src/app/_components/modeSwitch/index.tsx
+++ src/app/_components/modeSwitch/index.tsx
@@ -2,33 +2,44 @@
 
 import React from 'react'
 import styles from "./styles.module.css";
-import { Switch } from '@mui/material';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ModeSwitch = () => {
 
-  const toggleMode = () => {
-    const container:any = document.querySelector(':root');
-    const buttonDarkMode:any = document.getElementById('toggleDarkMode');
+  
 
+  const toggleMode = () => {
+
+    const icon:any = document.getElementsByClassName(styles.icon);
+    
+    const container:any = document.querySelector(':root');
     const dataTheme = (container as HTMLElement).getAttribute('data-theme')
 
     if(dataTheme === 'dark') {
       container.setAttribute('data-theme', 'light');
-      buttonDarkMode.innerHTML = 'Dark Mode';
+      icon[0].classList.add(styles.active);
+      icon[1].classList.remove(styles.active);
+
     } else {
       container.setAttribute('data-theme', 'dark');
-      buttonDarkMode.innerHTML = 'Light Mode';
+      icon[0].classList.remove(styles.active);
+      icon[1].classList.add(styles.active);
+
     }
   }
 
  
   return (
     <>
-      {/* <Switch checkedIcon={faMoon.icon}></Switch> */}
-
       <button className={styles.switch} id='toggleDarkMode' onClick={toggleMode}>
-        Light Mode 
+        <div className={styles.icon}>
+          <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
+        </div>
+        <div className={styles.icon+' '+styles.active}>
+          <FontAwesomeIcon icon={faSun}></FontAwesomeIcon>
+        </div>
       </button>
     </>
   )
