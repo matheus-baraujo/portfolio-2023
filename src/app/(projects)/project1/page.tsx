6936dbf789181828aef8e1762215dc645c5c7011
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2024-06-03 14:44:49 -0300

    changing projects

---
+++ src/app/(projects)/project1/page.tsx
@@ -0,0 +1,18 @@
+import styles from './style.module.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
+export default function Project1() {
+  return (
+      <main className={styles.main}>
+
+        <Container fluid="sm" style={{position: 'relative'}}>
+
+          <Row className='px-3 px-md-5'>
+            
+          </Row>
+        </Container>
+      </main>
+  )
+}
