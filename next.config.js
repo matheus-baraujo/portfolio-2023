e65dfea913093e1c5344646303fa8edaef8a3b91
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2026-05-05 12:36:53 -0300

    quick reference fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Isso gera a pasta 'out' ao rodar o build
+    basePath: '/portfolio-2023',
     images: {
       unoptimized: true, // O GitHub Pages não suporta a otimização de imagem nativa do Next.js
     },
